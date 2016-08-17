204f7c282c0b1354e1a1d09bbdaec8dbd3c053fc
norrisj <johntnorris@outlook.com>
2016-08-17 17:03:20 +0100

    Added new conflict content

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,36 @@
-const $hero = $('.conflict--hero');
-const origUrl = $hero.css('backgroundImage');
-const heroUrl = origUrl.replace('/img/', '/img/hero/');
+;(function loadHighResImage() {
+  const $hero = $('.conflict--hero');
 
-$.ajax({
-  url: heroUrl.replace('url("', '').replace('")', ''),
-  success: function () {
-    $hero.css('backgroundImage', heroUrl + ', ' + origUrl);
+  if ($hero.length > 0 && $(window).innerWidth() > 600) {
+    const origUrl = $hero.css('backgroundImage');
+    const heroUrl = origUrl.replace('/img/', '/img/hero/');
+
+    $.ajax({
+      url: heroUrl.replace('url("', '').replace('")', ''),
+      success: function () {
+        $hero.css('backgroundImage', heroUrl + ', ' + origUrl);
+      }
+    });
   }
-});
+}());
+
+;(function selectCurrentPage() {
+  let path = window.location.pathname;
+
+  if (path.endsWith('/')) {
+    path = path.substring(0, path.length - 1);
+  }
+
+  $(`a[href="${path}"]`).addClass('selected');
+}());
+
+
+;(function zoomImage() {
+  const $hero = $('.conflict--hero');
+
+  // no point zooming on a low res image
+  if ($hero.length > 0 && $(window).innerWidth() > 600) {
+    $hero.click(() => $hero.toggleClass('zoom'));
+  }
+}());
+
